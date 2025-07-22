b114a4f5210820403ac973cf2dacf91c28a47cf3
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-07-22 14:38:30 +0100

    feat: Create the main script

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+app.listen(PORT, () => {
+  console.log(`Server listening for requests at port: ${PORT}!`);
+});
