5ef5892615d60191a5bba03aa14a7961be915cda
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-07-23 15:21:50 +0100

    refactor: Send main element when users request the '/' route

--- app.ts
+++ app.ts
@@ -2,7 +2,17 @@ import express, { type Request, type Response } from "express";
 const app = express();
 const PORT = 3001;
 
-app.get("/", (req: Request, res: Response) => res.send("Hello, world!"));
+app.get("/", (req: Request, res: Response) =>
+  res.send(`
+    <main style="text-align:center;padding:30px 10vw;">
+      <h1>Welcome to the Postss Rest API server!</h1>
+      <p>Postss provides RESTful APIs for the Postss social media app.</p>
+      <h2>Postss Showcase</h2>
+      <p>Here is the app's address: <a href=${process.env.POSTSS_APP_URI}>Postss App</a></p>
+    </main>
+  `)
+);
+
 app.listen(PORT, () => {
   console.log(`Server listening for requests at port: ${PORT}!`);
 });
