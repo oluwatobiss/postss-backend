b8c904012d42acc32630959601f13711f77b9f24
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-10-03 18:06:40 +0100

    fix: Use the correct backend uri

--- passport/github.ts
+++ passport/github.ts
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 const optionsObject = {
   clientID: process.env.GITHUB_CLIENT_ID as string,
   clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-  callbackURL: `${process.env.NEXT_PUBLIC_BACKEND_URI}/login/github`,
+  callbackURL: `${process.env.BACKEND_URI}/login/github`,
 };
 
 passport.use(
