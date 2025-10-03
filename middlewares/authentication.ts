cbfba64c6daace7230eed647581206b7fd483467
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-10-03 16:19:13 +0100

    refactor: Use single statement if syntax

--- middlewares/authentication.ts
+++ middlewares/authentication.ts
@@ -4,9 +4,8 @@ import { verifyToken } from "../utils/jwt.ts";
 function authenticateUser(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers["authorization"];
   const userToken = authHeader && authHeader.split(" ")[1];
-  if (!userToken) {
+  if (!userToken)
     return res.status(401).json({ message: "No verification token provided" });
-  }
   const payload = verifyToken(userToken);
   if (!payload)
     return res.status(403).json({ message: "Invalid verification token" });
